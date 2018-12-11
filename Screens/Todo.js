b7548b9440e0b5c895b3333de2d3168acb267842
Zhilei Ong <ong_zhilei@hotmail.com>
2018-12-11 21:56:37 +0800

    Section header component added

--- Screens/Todo.js
+++ Screens/Todo.js
@@ -1,34 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView, FlatList, SectionList } from 'react-native';
 
 class Todo extends React.Component {
-    render ( ) {
+
+    renderRow = ({ item }) =>
+        (<View style={styles.heaeder}>
+            <Text>{item.title}</Text>
+        </View>);
+
+
+    renderSectionHeader = ({ section }) => 
+            <View style={styles.header}>
+                <Text>(section.title)</Text>
+            </View>;
+
+
+
+    render() {
         return (
-            <ScrollView>
-                <View style={StyleSheet.container1} />
-                <View style={StyleSheet.containter2} />
-                <View style={StyleSheet.container3} />
-                <View style={StyleSheet.container1} />
-                <View style={StyleSheet.containter2} />
-          </ScrollView>
-            );
+            <SectionList
+                sections={sections}
+                renderItem={this.renderRow}
+                renderSectionHeader={this.renderSectionHeader}
+            />
+
+        );
     }
 }
 
-const styles = StyleSheet.create{{
-    container1: {
-        padding: 100, 
-        backgroundColor: 'blue'
-    },
-    containter2: {
-        padding: 100,
-        backgroundColor: 'red'
-    },
-    containter3: {
-        padding: 100,
-        backgroundColor: 'yellow'
 
-}}
+const sections = [
+    {
+        id: 0,
+        title: 'Section 1',
+        data:
+            [
+                { id: 0, text: 'Item 1' },
+                { id: 1, text: 'Item 2' }
+            ]
+    },
 
+
+    { id: 1, title: 'Section 2', data: [{ id: 3, text: 'Item 4' }, { id: 4, text: 'Item 5' }] }
+];
+
+
+const styles = StyleSheet.create({
+    row: {
+        padding: 15,
+        marginBottom: 5,
+        backgroundColor: 'skyblue'
+    },
+    header: {
+        backgroundColor: 'coral'
+    }
+});
 
 export default Todo;
